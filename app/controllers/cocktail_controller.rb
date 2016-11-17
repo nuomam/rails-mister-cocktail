2d28197b136f4d4471e57146b9b1bf37036c6090
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-17 11:17:04 +0100

    Created Controllers & Routes

---
+++ app/controllers/cocktail_controller.rb
@@ -0,0 +1,13 @@
+class CocktailController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktail, only: [ :index, :new, :create, :show ] do
+    resources :dose, only: [ :new, :create, :destroy]
+  end
 end
