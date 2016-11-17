1b9ce9cc27a574d6e343870adce98eb0a8da41f0
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-17 14:13:32 +0100

    v0.1

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,6 +15,8 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @ingredients = Ingredient.all.order(:name)
   end
 
   private
