b96176b217b2347f271f6607431c11f5fb266590
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-18 15:09:32 +0100

    Add Modal and error message

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,7 @@
 class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
-  end
-
-  def new
-    @cocktail = Cocktail.new
+    @cocktail_new = Cocktail.new
   end
 
   def create
@@ -13,13 +10,15 @@ class CocktailsController < ApplicationController
       @dose = Dose.new
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/new"
+      flash[:alert] = "Your cocktail isn't created. Please give a name to your new cocktail."
+      redirect_to cocktails_path
     end
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
+    @cocktail_new = Cocktail.new
   end
 
   private
